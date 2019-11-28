a1281360cdd01e78534d1450e4c4e0b52fb38d7a
maride <git@maride.cc>
2019-11-28 18:48:54 +0100

    Fix 'DHCPv4 length 0 too short' error

--- ethernet/dhcpv4/dhcp.go
+++ ethernet/dhcpv4/dhcp.go
@@ -18,8 +18,15 @@ func HandleDHCPv4Packet(packet gopacket.Packet) error {
 	var dhcppacket layers.DHCPv4
 	var ethernetpacket layers.Ethernet
 
+	// For some reason I can't find an explanation for,
+	// 	packet.Layer(layers.LayerTypeDHCPv4).LayerContents(), which effectively is
+	// 	packet.Layers()[3].layerContents(), is empty, but
+	//  packet.Layers()[2].layerPayload() contains the correct DHCP packet.
+	// ... although both calls should return the same bytes.
+	// TODO: Open an Issue on github.com/google/gopacket
+
 	// Decode raw packet into DHCPv4
-	decodeDHCPErr := dhcppacket.DecodeFromBytes(packet.Layer(layers.LayerTypeDHCPv4).LayerContents(), gopacket.NilDecodeFeedback)
+	decodeDHCPErr := dhcppacket.DecodeFromBytes(packet.Layers()[2].LayerPayload(), gopacket.NilDecodeFeedback)
 	if decodeDHCPErr != nil {
 		// Encountered an error during decoding, most likely a broken packet
 		return decodeDHCPErr
